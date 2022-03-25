27f796ff47bd14c478dad453d02cbdcfa86d2ef3
Oninoni <jan@cheap-purple.com>
2022-03-25 15:42:51 +0100

    Loader

---
+++ lua/autorun/stm_loader_transporter.lua
@@ -0,0 +1,22 @@
+---------------------------------------
+---------------------------------------
+--        Star Trek Utilities        --
+--                                   --
+--            Created by             --
+--       Jan 'Oninoni' Ziegler       --
+--                                   --
+-- This software can be used freely, --
+--    but only distributed by me.    --
+--                                   --
+--    Copyright © 2021 Jan Ziegler   --
+---------------------------------------
+---------------------------------------
+
+---------------------------------------
+--   Star Trek Transporter | Loader  --
+---------------------------------------
+
+Star_Trek = Star_Trek or {}
+Star_Trek.Modules = Star_Trek.Modules or {}
+
+Star_Trek.Modules["mobile_transporter"] = true
